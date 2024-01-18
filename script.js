65a9fdaa70318816e09696a471c7c6af988ae063
Laurence <llaurence689@gmail.com>
2024-01-18 17:39:41 +0630

    Implement pseudo and logic into script
    (writing code)

---
+++ script.js
@@ -0,0 +1,11 @@
+function getComputerChoice() {
+    let randomChoice =  Math.floor(Math.random() * 100) + 1;
+
+    if (randomChoice < 33) {
+        return 'Rock';
+    } else if(randomChoice < 66) {
+        return 'Paper';
+    } else {
+        return 'Scissors'
+    }
+}
