67658fd94d2d889bc5c7d0d13146d9762a74b31f
Laurence <llaurence689@gmail.com>
2024-01-19 10:50:39 +0630

    Plan and explain for round

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function getComputerChoice() {
     let randomChoice =  Math.floor(Math.random() * 100) + 1;
 
-    if (randomChoice < 33) {
+    if (randomChoice < 33.33333333333333) {
         return 'Rock';
-    } else if(randomChoice < 66) {
+    } else if(randomChoice < 66.66666666666667) {
         return 'Paper';
     } else {
         return 'Scissors';
