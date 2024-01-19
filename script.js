2f0c621e9e8eda321cb789ac4e4cd3620a36e2cf
Laurence <llaurence689@gmail.com>
2024-01-19 11:26:45 +0630

    Test algorithm logic

--- script.js
+++ script.js
@@ -2,12 +2,53 @@ function getComputerChoice() {
     let randomChoice =  Math.floor(Math.random() * 100) + 1;
 
     if (randomChoice < 33.33333333333333) {
-        return 'Rock';
+        return 'rock';
     } else if(randomChoice < 66.66666666666667) {
-        return 'Paper';
+        return 'paper';
     } else {
-        return 'Scissors';
+        return 'scissors';
     }
 }
 
 console.log(getComputerChoice());
+
+
+function playRound(playerSelection,computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    switch (playerSelection) {
+        case computerSelection:
+            console.log('It is a tie, my friends! Restarting round.');
+            playRound(playerSelection,computerSelection);
+            break;
+
+        case computerSelection === 'rock' && playerSelection === 'paper':
+            console.log('Player victory, paper beats rock!');
+            break;
+
+        case computerSelection === 'rock' && playerSelection === 'scissors':
+            console.log('Computer victory, rock beats scissors boo!');
+            break;
+
+        case computerSelection === 'paper' && playerSelection === 'rock':
+            console.log('Computer victory, their paper beats your rock!');
+            break;
+
+        case computerSelection === 'paper' && playerSelection === 'scissors':
+            console.log('Player victory! Player\'s selection beat the heck outta the computer\'s');
+            break;
+
+        case computerSelection === 'scissors' && playerSelection === 'rock':
+            console.log('Player victory, rock beats scissors!');
+            break;
+    
+        case computerSelection === 'scissors' && playerSelection === 'paper':
+            console.log('Computer victory, rock beats scissors boo!');
+            break;
+
+    }
+}
+
+console.log(playRound('ROck',getComputerChoice()));
+console.log(playRound('PaPEr',getComputerChoice()));
+console.log(playRound('scISSoRS',getComputerChoice()));
